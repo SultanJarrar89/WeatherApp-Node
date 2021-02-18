7fb3c877dd4adb8d03f4ae53a306872035f5dcc8
sultan <sultanjarrar89@gmail.com>
2021-02-18 02:04:58 -0800

    fetching weather data

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const request = require('request')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.API}&query=37.7749,122.4194`
+const url = `http://api.weatherstack.com/current?access_key=${process.env.API}&query=37.7749,122.4194&units=f`
 
-request({ url }, (err, res) => {
-  const data = JSON.parse(res.body)
-  console.log(data.current)
+request({ url, json: true }, (err, res) => {
+  let current = res.body.current
+  console.log(current)
+  console.log(
+    `${current.weather_descriptions[0]}.It is currently ${current.temperature} degressout,There is a ${current.precip} % chance of rain `
+  )
 })
